837cd1f9417ab6ffa26f88f340ad3d05560e0344
Sander in 't Hout <sander@inthout.me>
2024-11-03 20:43:54 +0100

    Add first bits of reusable AppShell Apply HHs colors to theme, apply Lato font

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import QuizComponent from '@/components/QuizComponent';
+import AppBarButton from '@/components/AppBar/AppBarButton';
+import AppShell from './components/AppShell';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import "@fontsource/lato";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Lato',
+    fontSize: 18,
+  },
+  palette: {
+    primary: {
+      main: '#223343',
+    },
+    secondary: {
+      main: '#9ea601',
+    },
+  },
+});
+
 
 const App = () => {
   return (
-      <div>
-        <h1>Quiz Generator</h1>
-        <QuizComponent />
-      </div>
-  );
+    <ThemeProvider theme={theme}>
+      <AppShell>{{
+        appBarButtons: [
+          <AppBarButton onClick={() => alert("test!")}>Begin opnieuw</AppBarButton>,
+        ],
+        body: <>
+          <h2>Nieuwe quiz</h2>
+        </>
+      }}</AppShell>
+    </ThemeProvider>
+  )
 };
 
 export default App;
---
+++ src/components/AppShell.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Divider from '@mui/material/Divider';
+import AppBarButton from './AppBar/AppBarButton';
+
+export default function AppShell({ children: {appBarButtons, body} }) {
+    return (
+        <>
+            <Divider aria-hidden="true" sx={{height: "3px", backgroundColor: "primary.main"}}></Divider>
+            <Container sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                <div style={{ display: 'flex', alignItems: 'center'}}>
+                    <h1>H-AI</h1>
+                    <img alt="Logo van De Haagse Hogeschool" src='hhs-logo-groen.png' style={{ height: '64px', margin: '0 24px' }}></img>
+                </div>
+                <p>Welkom bij H-AI, de applicatie die assisteert met het maken van korte quizzes</p>
+            </Container>
+            <AppBar position="static">
+                <Container>
+                    <Toolbar disableGutters="true">
+                        { appBarButtons }
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            <Container>
+                { body }
+            </Container>
+        </>
+    );
+};
