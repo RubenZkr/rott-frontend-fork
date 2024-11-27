9c11b692e0e1c3a8d821f2c4d7e29b2f5175ad34
Sander in 't Hout <sander@inthout.me>
2024-11-27 23:00:05 +0100

    Add Brightspace CSV download button, add first bits of regenerate

--- src/api/QuizApi.js
+++ src/api/QuizApi.js
@@ -1,7 +1,7 @@
-import {fetchApi, postFormData} from "@/api/BaseApi";
+import {fetchApi, postFormData, getBlob} from "@/api/BaseApi";
 
 export const generateQuiz = (formData) => postFormData('/quizzes/generate', formData);
 export const getQuiz = (quizUuid) => fetchApi(`/quizzes/${quizUuid}`);
-export const exportQuiz = (quizUuid) => fetchApi(`/quizzes/${quizUuid}/export`);
+export const exportQuiz = (quizUuid) => getBlob(`/quizzes/${quizUuid}/export`);
 export const regenerateQuestion = (quizUuid, questionId) => fetchApi(`/quizzes/${quizUuid}/regenerate_question/${questionId}`, 'POST');
 export const getQuizProgress = (quizUuid) => fetchApi(`/quizzes/${quizUuid}/progress`);
--- src/pages/ViewQuiz.js
+++ src/pages/ViewQuiz.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import AppShell from '@/components/AppShell';
 import "@fontsource/lato";
-import { Box, CircularProgress, Typography } from '@mui/material';
-import { getQuiz } from '@/api/QuizApi';
+import { Box, Button, CircularProgress, Typography } from '@mui/material';
+import { getQuiz, regenerateQuestion, exportQuiz } from '@/api/QuizApi';
 import { useNavigate, useParams } from 'react-router-dom';
-import { ArrowBack, BackHand, CheckBox, CheckBoxOutlineBlank } from '@mui/icons-material';
+import { ArrowBack, CheckBox, CheckBoxOutlineBlank, Download, Save } from '@mui/icons-material';
 import AppBarButton from '@/components/AppBar/AppBarButton';
 
 export default function ViewQuiz() {
@@ -25,6 +25,10 @@ export default function ViewQuiz() {
 
         fetchData().catch(console.error); // TODO: better error handling
     }, [quizUuid]);
+
+    function startQuestionRegenerate(questionId) {
+        regenerateQuestion(quiz.id, questionId);
+    }
 
     function getAnswers(question) {
         if (question.type === 'MC') {
@@ -57,10 +61,22 @@ export default function ViewQuiz() {
         }
     }
 
+    async function downloadBrightspaceCsv() {
+        let brightspaceCsvBlob = await exportQuiz(quizUuid);
+        var fileURL = URL.createObjectURL(brightspaceCsvBlob);
+        var fileLink = document.createElement('a');
+        fileLink.href = fileURL;
+        fileLink.download = `Quiz-${new Date().toISOString()}.csv`;
+        fileLink.click();
+    }
+
     return (
         <AppShell>{{
             appBarButtons: [
-                <AppBarButton onClick={() => { navigate('/') }}><ArrowBack />&nbsp;Begin opnieuw</AppBarButton>
+                <AppBarButton onClick={() => { navigate('/') }}><ArrowBack />&nbsp;Begin opnieuw</AppBarButton>,
+                <div style={{marginLeft: 'auto'}}>
+                    <AppBarButton onClick={() => { downloadBrightspaceCsv() }}><Download />&nbsp;Download .csv voor Brightspace</AppBarButton>
+                </div>
             ],
             body: downloadingQuiz || !quiz ? <Box sx={{ display: 'flex', alignItems: 'center' }}>
                 <CircularProgress />
@@ -71,10 +87,10 @@ export default function ViewQuiz() {
             <>
                 <h1>Uw quiz:</h1>
                 <h2>Onderwerp: {quiz.title}</h2>
-                
                     <ol>
                         {quiz.questions.map((question, i) =>
                             <li>
+                                {/* <Button onClick={() => startQuestionRegenerate(question.id)}>Regenerate</Button> */}
                                 {question.question_text}
                                 <ul>
                                     {getAnswers(question)}
